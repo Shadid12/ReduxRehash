b0ad50e6596e364fd7ac95af79d97d0cf736ff95
Shadid Haque <shaque@intouchinsight.com>
2017-11-17 01:27:59 -0500

    all the simple reducers

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
+import { addCharacterbyId } from "./actions/index";
 
 const store = createStore(rootReducer);
 console.log(store.getState());
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(addCharacterbyId(3));
 
 ReactDOM.render(
     <Provider store={store}>
