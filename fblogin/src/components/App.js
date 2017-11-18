f083f89df4046a57666b6d1732ebc64b085d4d5b
shadid12 <shadidhaque2012@gmail.com>
2017-11-17 23:49:53 -0500

    working google auth

--- fblogin/src/components/App.js
+++ fblogin/src/components/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 class App extends React.Component {
     render () {
         return (
-            <FacebookLogin
-                appId="470840129963641"
-                autoLoad={true}
-                fields="name,email,picture"
-                onClick={(res) => {console.log(res)}}
-                callback={(res) => console.log('response', res)} />
+            <div>
+                <FacebookLogin
+                    appId="470840129963641"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    onClick={(res) => {console.log(res)}}
+                    callback={(res) => console.log('response', res)} />
+
+                <GoogleLogin
+                    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={(res) => {console.log('success',res)}}
+                    onFailure={(res) => {console.log(res)}}
+                />
+            </div>
         )
     }
 }
